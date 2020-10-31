21a569eaa1171188ec1fc495b659c83b4bc26c32
Apoorva Gupta <apoorvagupt30@outlook.com>
2020-10-31 14:05:43 +0530

    minor changes

--- action_page.php
+++ action_page.php
@@ -1,7 +1,7 @@
 <?php
 $email = $_POST['username'];
 $password = $_POST['password'];
-
+echo print_r($_POST,true);
 $con=mysqli_connect("localhost","root","","blog");
 
     // Check connection
@@ -10,13 +10,23 @@ $con=mysqli_connect("localhost","root","","blog");
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     else{
-      echo"Successs!!!!!";
+      echo"Successful Connection";
     }
 
-  $res = mysqli_query($con, "SELECT * FROM user_login WHERE email = '$email' AND password = '$password'");
-  while(!$res){
-    die("Invalid query: " . mysqli_error());
+  $res = mysqli_query($con, "SELECT * FROM user_details WHERE email = '$email' AND password = '$password'");
+  $count=mysqli_num_rows($res);
+  if($count==1){
+    echo "Successfully logged in!";
   }
+
+  else {
+    echo "<script type='text/javascript'>alert('Wrong Username or Password');
+    window.location='login.html';
+    </script>";
+    }
+    
+  
+  
 
   
   
